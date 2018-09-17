a8302e8f777bdf4502b0c2016ba2eafedd6f24ac
Jane <jane.edwards.mcnear@gmail.com>
2018-09-17 16:43:37 -0700

    Got show to work and added links to index.html.erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,24 @@
 TASKS = [
-  { one: "Grocery shopping", status: :complete},
-  { two: "Take out the trash", status: :inprogress},
-  { three: "Eat pizza", status: :incomplete},
-  { four: "Do homework" status: :incomplete},
+  { name: "one", description: "Grocery shopping", completion_date: "2018-01-01"},
+  { name: "two", description: "Take out the trash", completion_date: "2018-01-01"},
+  { name: "three", description: "Eat pizza", completion_date: "2018-01-01"},
+  { name: "four", description: "Do homework", completion_date: "2018-01-01"}
 ]
 
 class TasksController < ApplicationController
   def index
     @tasks = TASKS
   end
+
+  def show
+    id = params[:id]
+    @task = TASKS.find do |task|
+      task[:name] == id
+    end
+
+    # if @task.nil?
+    #
+    # end
+  end
+
 end

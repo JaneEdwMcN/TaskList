c0d42cadeeadce6670f384538f1f362b8687a165
Jane <jane.edwards.mcnear@gmail.com>
2018-09-17 14:19:45 -0700

    Added array of hashes to tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,12 @@
+TASKS = [
+  { one: "Grocery shopping", status: :complete},
+  { two: "Take out the trash", status: :inprogress},
+  { three: "Eat pizza", status: :incomplete},
+  { four: "Do homework" status: :incomplete},
+]
+
 class TasksController < ApplicationController
   def index
+    @tasks = TASKS
   end
 end

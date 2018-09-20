246ab35e699741942fe2d641f2d7eeb58be2ce3e
Jane <jane.edwards.mcnear@gmail.com>
2018-09-19 18:09:33 -0700

    Finished Wave 4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,4 @@
+require 'date'
 class TasksController < ApplicationController
   def index
     @tasks = Task.all.order(:name)
@@ -9,23 +10,51 @@ class TasksController < ApplicationController
   end
 
   def new
-
+    @task = Task.new
   end
 
   def create
-
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @task = Task.find(params[:id].to_i)
   end
 
   def update
-
+    @task = Task.find(params[:id].to_i)
+    @task.name = params[:task][:name]
+    @task.description = params[:task][:description]
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    id = params[:id]
+    @task = Task.find_by(id: id)
+    if @task.destroy
+      redirect_to root_path
+    end
+  end
 
+  def mark_complete
+    id = params[:id]
+    @task = Task.find_by(id: id)
+
+    if @task.completion_date == nil
+      @task.update(completion_date: Date.today)
+    else
+      @task.update(completion_date: nil)
+    end
+    redirect_to root_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  root "tasks#index"
+
   get '/tasks', to: 'tasks#index', as: 'tasks'
+
   get	'/tasks/new',	to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
+
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
   get	'/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  patch '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete_task'
   patch '/tasks/:id', to: 'tasks#update'
+
   delete '/tasks/:id', to: 'tasks#destroy'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
